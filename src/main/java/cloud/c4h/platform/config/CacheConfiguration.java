35478386e965560ab8790856b4b6fdfc5a06ba84
Lars <lars@inidus.com>
2018-03-28 14:18:55 +0100

    Rename configuration strings

--- src/main/java/cloud/c4h/platform/config/CacheConfiguration.java
+++ src/main/java/cloud/c4h/platform/config/CacheConfiguration.java
@@ -53,7 +53,7 @@ public class CacheConfiguration {
     public HazelcastInstance hazelcastInstance(JHipsterProperties jHipsterProperties) {
         log.debug("Configuring Hazelcast");
         Config config = new Config();
-        config.setInstanceName("operoncloudplatform");
+        config.setInstanceName("c4hcloudplatform");
         config.getNetworkConfig().setPort(5701);
         config.getNetworkConfig().setPortAutoIncrement(true);
 
